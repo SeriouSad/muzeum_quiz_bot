6cfb47a158565519fdfce1a371eb445868c60b85
Konstantin Dolgopolov <60754052+SeriouSad@users.noreply.github.com>
2024-09-13 12:34:38 +0300

    upd7

--- museum_bot/models.py
+++ museum_bot/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class TgUser(models.Model):
     tg_id = models.CharField(verbose_name="Id пользователя в telegram", max_length=255)
-    username = models.CharField(max_length=255, verbose_name="Никнейм")
+    username = models.CharField(max_length=255, verbose_name="Никнейм", null=True, default=None)
     fio = models.CharField(max_length=255, verbose_name="ФИО")
     phone_number = models.CharField(max_length=255, verbose_name="Номер телефона")
     email = models.CharField(max_length=255, verbose_name="email")
