3a8aa730b2dfb0d9071b3e1df1141e2f853b7ebb
Konstantin Dolgopolov <60754052+SeriouSad@users.noreply.github.com>
2024-10-25 12:39:02 +0300

    delete

--- msr_bot/bot/keyboards.py
+++
@@ -1,5 +0,0 @@
-from django.db.utils import ProgrammingError
-from telebot.types import KeyboardButton, InlineKeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup
-
-start_kb = InlineKeyboardMarkup()
-start_kb.add(InlineKeyboardButton("Начать", callback_data="start"))
--- msr_bot/bot/states.py
+++
@@ -1,8 +0,0 @@
-from telebot.handler_backends import State, StatesGroup
-
-
-class StartStates(StatesGroup):
-    start = State()
-
-class AnswerStates(StatesGroup):
-    waiting_for_answer = State()
